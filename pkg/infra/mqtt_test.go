f4d6db4fe0b6e6e4b05b546ff8a1fe6f6cc8497d
koke <niwaniwa@users.noreply.github.com>
2023-11-25 20:18:27 +0900

    add mqtt test code

---
+++ pkg/infra/mqtt_test.go
@@ -0,0 +1,38 @@
+package infra
+
+import (
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"testing"
+	"time"
+)
+
+func TestConnect(t *testing.T) {
+	CreateMQTTClient("localhost:1883")
+	CloseAll()
+}
+
+func TestMQTTPublishAndSubscribe(t *testing.T) {
+	CreateMQTTClient("localhost:1883")
+
+	defer CloseAll()
+
+	// Subscribe
+	receivedMessage := make(chan string)
+
+	Subscribe("test/topic", func(message mqtt.Message) {
+		receivedMessage <- string(message.Payload())
+	})
+
+	// Publish
+	testMessage := "Hello MQTT"
+	Publish("test/topic", testMessage, 0)
+
+	select {
+	case message := <-receivedMessage:
+		if message != testMessage {
+			t.Errorf("Expected message %q but got %q", testMessage, message)
+		}
+	case <-time.After(time.Second * 5):
+		t.Error("Timeout waiting for the message")
+	}
+}
